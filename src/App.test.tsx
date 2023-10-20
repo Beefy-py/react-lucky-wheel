156ad38696a4dd78a136d970ae6c2f6351f7fbd2
Beefy-py <hoftkenny@gmail.com>
2023-10-19 23:59:10 -0300

    refactor: :fire: removed useless files
    need to write a very descriptive readme

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders spin wheel', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/spin the wheel/i);
-  expect(linkElement).toBeInTheDocument();
-});
