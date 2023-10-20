05ea885ceffb635863b4212731b9674febda86ad
Beefy-py <hoftkenny@gmail.com>
2023-10-19 23:54:17 -0300

    build: :building_construction: Made project ready to be published to npm

---
+++ src/interfaces/index.ts
@@ -0,0 +1,6 @@
+export { WheelInterface as WheelInterface } from "./wheel.interface";
+export { WheelSegmentInterface as WheelSegmentInterface } from "./wheelSegment.interface";
+export { SpinButtonInterface as SpinButtonInterface } from "./spinButton.interface";
+export { PinInterface as PinInterface } from "./pin.interface";
+export { ExtendedWheelSegmentInterface as ExtendedWheelSegmentInterface } from "./extendedWheelSegment.interface";
+export { ArrowSpinnerButtonInterface as ArrowSpinnerButtonInterface } from "./arrowSpinButton.interface";
